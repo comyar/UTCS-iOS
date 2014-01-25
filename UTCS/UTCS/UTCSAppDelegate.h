c3e7ac094c14ba68f3f37685368cf16566af2ef6
Henri Sweers <pandanomic@gmail.com>
2014-01-25 06:15:48 -0600

    Added initial files

---
+++ UTCS/UTCS/UTCSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UTCSAppDelegate.h
+//  UTCS
+//
+//  Created by Henri Sweers on 1/23/14.
+//  Copyright (c) 2014 UTCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UTCSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UTCS/UTCS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UTCS
+//
+//  Created by Henri Sweers on 1/23/14.
+//  Copyright (c) 2014 UTCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UTCSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UTCSAppDelegate class]));
+    }
+}
