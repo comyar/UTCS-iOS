10f2e28454d6a8f49b8633cf5b492df38258b346
Comyar <comyarzaheri@utexas.edu>
2014-03-22 21:04:18 -0500

    fixed parallax blur nav bar header in news
    Former-commit-id: b457964259d6a53f8f5ec2ef24f24de232941c97
    Former-commit-id: 90ea71af73c302e1f964f837a16cca4069e2d051
    Former-commit-id: 3bb54bd62917c0cd7758e952a8e7816c3bc3842a

--- UTCS/UTCS/Classes/ViewControllers/UTCSNewsDetailViewController.m
+++ UTCS/UTCS/Classes/ViewControllers/UTCSNewsDetailViewController.m
@@ -12,13 +12,10 @@
 #import "UITextView+CZTextViewHeight.h"
 #import "UTCSNewsStory.h"
 
-#import "UTCSParallaxHeaderScrollView.h"
-
 
 @interface UTCSNewsDetailViewController ()
 
-@property (nonatomic) UTCSParallaxHeaderScrollView      *parallaxScrollView;
-
+@property (nonatomic) UIScrollView                      *scrollView;
 @property (strong, nonatomic) UITextView                *contentTextView;
 
 @property (nonatomic) NSArray                           *defaultHeaders;
@@ -30,6 +27,10 @@
 
 @property (nonatomic) UIView                            *headerContainer;
 
+@property (nonatomic) UILabel                           *headerTitleLabel;
+
+@property (nonatomic) CAShapeLayer                      *headerMask;
+
 @end
 
 @implementation UTCSNewsDetailViewController
@@ -37,7 +38,9 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
-        self.view.backgroundColor = [UIColor blackColor];
+        
+        
+        self.view.backgroundColor = [UIColor whiteColor];
         self.defaultHeaders = @[[UIImage imageNamed:@"header"]];
         self.defaultBlurredHeaders = @[[UIImage imageNamed:@"blurredHeader"]];
         
@@ -46,6 +49,8 @@
         self.blurredImageView = [[UIImageView alloc]initWithImage:self.defaultBlurredHeaders[0]];
         self.blurredImageView.alpha = 0.0;
         
+        
+        
         self.headerContainer = [UIView new];
         self.headerContainer.backgroundColor = [UIColor clearColor];
         
@@ -53,10 +58,32 @@
         [self.headerContainer addSubview:self.headerImageView];
         [self.headerContainer addSubview:self.blurredImageView];
         
-        self.parallaxScrollView = [[UTCSParallaxHeaderScrollView alloc]initWithFrame:self.view.bounds headerView:self.headerImageView];
-        self.parallaxScrollView.backgroundColor = [UIColor clearColor];
-        [self.view addSubview:self.parallaxScrollView];
-
+        [self.view addSubview:self.headerContainer];
+        
+        
+        
+        self.headerTitleLabel = [[UILabel alloc]initWithFrame:self.headerContainer.bounds];
+        self.headerTitleLabel.numberOfLines = 0;
+        self.headerTitleLabel.adjustsFontSizeToFitWidth = YES;
+        self.headerTitleLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+        self.headerTitleLabel.shadowOffset = CGSizeMake(0, 1);
+        self.headerTitleLabel.shadowColor = [UIColor colorWithWhite:1.0 alpha:0.5];
+        [self.headerContainer addSubview:self.headerTitleLabel];
+        
+        
+        [self.headerContainer addSubview:self.headerTitleLabel];
+        
+        self.scrollView = [[UIScrollView alloc]initWithFrame:self.view.bounds];
+        self.scrollView.alwaysBounceVertical = YES;
+        self.scrollView.delegate = self;
+        [self.view addSubview:self.scrollView];
+        
+        self.contentTextView = [[UITextView alloc]initWithFrame:CGRectMake(0.0, 0.0, self.view.width, 0.0)];
+        self.contentTextView.scrollEnabled = NO;
+        self.contentTextView.editable = NO;
+        [self.scrollView addSubview:self.contentTextView];
+        
+        self.headerMask = [CAShapeLayer layer];
     }
     return self;
 }
@@ -66,18 +93,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.edgesForExtendedLayout = UIRectEdgeAll;
     self.view.backgroundColor = [UIColor whiteColor];
     
-    }
+}
 
 - (void)updateWithNewsStory:(UTCSNewsStory *)newsStory
 {
-
+    CGFloat height = [newsStory.title boundingRectWithSize:self.headerTitleLabel.bounds.size options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName: self.headerTitleLabel.font} context:nil].size.height;
+    
+//    self.headerTitleLabel.y = self.headerContainer.height - height;
+    self.headerTitleLabel.text = newsStory.title;
+    self.contentTextView.attributedText = newsStory.attributedContent;
+    self.contentTextView.y = self.headerContainer.height;
+    self.contentTextView.height = [self.contentTextView heightWithText];
+    self.scrollView.contentSize = CGSizeMake(self.view.width, self.contentTextView.height + self.headerContainer.height);
+    [self.scrollView scrollRectToVisible:CGRectZero animated:YES];
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
-    self.blurredImageView.alpha = MIN(1.0,  10.0 * MAX(scrollView.contentOffset.y / self.view.height, 0.0));
+    if(scrollView == self.scrollView) {
+        if(scrollView.contentOffset.y > 0) {
+            if(self.scrollView.contentOffset.y < self.headerContainer.height - 64.0) {
+                self.headerContainer.y = -0.5 * scrollView.contentOffset.y;
+                [self.view sendSubviewToBack:self.headerContainer];
+                self.headerContainer.layer.masksToBounds = NO;
+                self.headerContainer.layer.mask = nil;
+            } else {
+                self.headerMask.path = [[UIBezierPath bezierPathWithRect:CGRectMake(self.headerContainer.x, -self.headerContainer.y, self.headerContainer.width, 64.0)]CGPath];
+                [self.view bringSubviewToFront:self.headerContainer];
+                self.headerContainer.layer.mask = self.headerMask;
+            }
+        } else {
+            self.headerContainer.y = 0.0;
+        }
+        self.blurredImageView.alpha = MIN(1.0,  10.0 * MAX(scrollView.contentOffset.y / self.view.height, 0.0));
+    }
 }
 
 #pragma mark Overridden Setters
