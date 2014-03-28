958f96c14e2c256f7372f7e04dd0b771d211bbe0
Comyar <comyarzaheri@utexas.edu>
2014-03-28 05:08:08 -0500

    added new event view controller
    Former-commit-id: 406b0dcbb98b419109ff2444a5d894df2c8b8ba3
    Former-commit-id: 9d9fe039979dbb27c7227e63a6b13800b2300fc2
    Former-commit-id: fccb8f7acd5fc03646f8adc80803a90abd8edfe3

---
+++ UTCS/UTCS/Classes/ViewControllers/UTCSEventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  UTCSEventsViewController.h
+//  UTCS
+//
+//  Created by Comyar Zaheri on 3/28/14.
+//  Copyright (c) 2014 UTCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UTCSEventsViewController : UIViewController
+
+@end
