51c67d56839c6faabec3560009907be27be9dd0e
Comyar <comyarzaheri@utexas.edu>
2014-04-02 10:51:13 -0500

    moved initialization of subviews to init for menu for performance
    Former-commit-id: 98bf41425dbe0c82488074616648f04c130ed080
    Former-commit-id: 4e6ba26af2fb8bd0b69aa4cd752dc1750c8f87c8
    Former-commit-id: a7c102ebba4895b5e6b8e16a320a8ba5ba66f2a3

--- UTCS/UTCS/Classes/ViewControllers/UTCSMenuViewController.m
+++ UTCS/UTCS/Classes/ViewControllers/UTCSMenuViewController.m
@@ -44,6 +44,61 @@
     if (self =[super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
         self.menuOptions = @[@"News", @"Events", @"Directory", @"Labs", @"Settings"];
         self.notificationsViewController = [UTCSNotificationsViewController new];
+        self.title = @"Menu";
+        
+        self.edgesForExtendedLayout = UIRectEdgeNone;
+        self.view.backgroundColor = [UIColor colorWithWhite:0.1 alpha:1.0];
+        
+        self.tableView = [[UITableView alloc]initWithFrame:CGRectMake(0.0, 0.0, 0.75 * self.view.width, self.view.height)
+                                                     style:UITableViewStylePlain];
+        self.tableView.dataSource = self;
+        self.tableView.delegate = self;
+        self.tableView.rowHeight = 60;
+        self.tableView.contentInset = UIEdgeInsetsMake(0.05 * self.view.height, 0, 0, 0);
+        self.tableView.backgroundColor = [UIColor clearColor];
+        self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        self.tableView.width = 0.75 * self.view.width;
+        [self.view addSubview:self.tableView];
+        
+        self.facebookButton = ({
+            UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+            [button addTarget:self action:@selector(didTouchUpInsideButton:) forControlEvents:UIControlEventTouchUpInside];
+            UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"facebook"]];
+            button.frame = CGRectMake(self.view.width - 128, 0.675 * self.view.height, 44.0, 44.0);
+            imageView.frame = CGRectMake(0.0, 0.0, 44.0, 44.0);
+            button.showsTouchWhenHighlighted = YES;
+            [button addSubview:imageView];
+            button.alpha = 0.5;
+            button;
+        });
+        [self.view addSubview:self.facebookButton];
+        
+        self.twitterButton = ({
+            UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+            [button addTarget:self action:@selector(didTouchUpInsideButton:) forControlEvents:UIControlEventTouchUpInside];
+            UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"twitter"]];
+            button.frame = CGRectMake(self.view.width - 64, 0.675 * self.view.height, 44.0, 44.0);
+            imageView.frame = CGRectMake(0.0, 0.0, 44.0, 44.0);
+            button.showsTouchWhenHighlighted = YES;
+            [button addSubview:imageView];
+            button.alpha = 0.5;
+            button;
+        });
+        [self.view addSubview:self.twitterButton];
+        
+        self.notificationsButton = ({
+            UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+            [button addTarget:self action:@selector(didTouchUpInsideButton:) forControlEvents:UIControlEventTouchUpInside];
+            button.frame = CGRectMake(self.view.width - 64.0, 0.05 * self.view.height, 44.0, 44.0);
+            UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"notifications"]];
+            imageView.frame = CGRectMake(0.0, 0.0, 25.0, 25.0);
+            imageView.center = CGPointMake(0.5 * button.width, 0.5 * button.height);
+            imageView.alpha = 0.5;
+            [button addSubview:imageView];
+            button.showsTouchWhenHighlighted = YES;
+            button;
+        });
+        [self.view addSubview:self.notificationsButton];
     }
     return self;
 }
@@ -53,61 +108,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.title = @"Menu";
     
-    self.edgesForExtendedLayout = UIRectEdgeNone;
-    self.view.backgroundColor = [UIColor colorWithWhite:0.1 alpha:1.0];
-    
-    self.tableView = [[UITableView alloc]initWithFrame:CGRectMake(0.0, 0.0, 0.75 * self.view.width, self.view.height)
-                                                 style:UITableViewStylePlain];
-    self.tableView.dataSource = self;
-    self.tableView.delegate = self;
-    self.tableView.rowHeight = 60;
-    self.tableView.contentInset = UIEdgeInsetsMake(0.05 * self.view.height, 0, 0, 0);
-    self.tableView.backgroundColor = [UIColor clearColor];
-    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-    self.tableView.width = 0.75 * self.view.width;
-    [self.view addSubview:self.tableView];
-    
-    self.facebookButton = ({
-        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-        [button addTarget:self action:@selector(didTouchUpInsideButton:) forControlEvents:UIControlEventTouchUpInside];
-        UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"facebook"]];
-        button.frame = CGRectMake(self.view.width - 128, 0.675 * self.view.height, 44.0, 44.0);
-        imageView.frame = CGRectMake(0.0, 0.0, 44.0, 44.0);
-        button.showsTouchWhenHighlighted = YES;
-        [button addSubview:imageView];
-        button.alpha = 0.5;
-        button;
-    });
-    [self.view addSubview:self.facebookButton];
-    
-    self.twitterButton = ({
-        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-        [button addTarget:self action:@selector(didTouchUpInsideButton:) forControlEvents:UIControlEventTouchUpInside];
-        UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"twitter"]];
-        button.frame = CGRectMake(self.view.width - 64, 0.675 * self.view.height, 44.0, 44.0);
-        imageView.frame = CGRectMake(0.0, 0.0, 44.0, 44.0);
-        button.showsTouchWhenHighlighted = YES;
-        [button addSubview:imageView];
-        button.alpha = 0.5;
-        button;
-    });
-    [self.view addSubview:self.twitterButton];
-    
-    self.notificationsButton = ({
-        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-        [button addTarget:self action:@selector(didTouchUpInsideButton:) forControlEvents:UIControlEventTouchUpInside];
-        button.frame = CGRectMake(self.view.width - 64.0, 0.05 * self.view.height, 44.0, 44.0);
-        UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"notifications"]];
-        imageView.frame = CGRectMake(0.0, 0.0, 25.0, 25.0);
-        imageView.center = CGPointMake(0.5 * button.width, 0.5 * button.height);
-        imageView.alpha = 0.5;
-        [button addSubview:imageView];
-        button.showsTouchWhenHighlighted = YES;
-        button;
-    });
-    [self.view addSubview:self.notificationsButton];
 }
 
 - (void)viewWillAppear:(BOOL)animated
