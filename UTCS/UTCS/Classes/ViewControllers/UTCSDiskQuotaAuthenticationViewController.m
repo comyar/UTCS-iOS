f3cb8d15f577cf446c3dbc5d9f99b8508b8a169e
Comyar <comyarzaheri@utexas.edu>
2014-04-04 11:53:00 -0500

    began implementing disk quota auth
    Former-commit-id: 9b466e69b1b6e7bb2a07aaac8c704e202bf24455
    Former-commit-id: 8f70fc6ad6037265ddd3cecdfcc57b96e7dce9f5
    Former-commit-id: 94b607ac23aff641213a0d2bd59542b71761e3bf

--- UTCS/UTCS/Classes/ViewControllers/UTCSDiskQuotaAuthenticationViewController.m
+++ UTCS/UTCS/Classes/ViewControllers/UTCSDiskQuotaAuthenticationViewController.m
@@ -8,13 +8,17 @@
 
 #import "UTCSDiskQuotaAuthenticationViewController.h"
 #import "UTCSMenuButton.h"
+#import "UIView+CZPositioning.h"
+#import "UTCSSSHManager.h"
+#import "UIView+Shake.h"
 
 @interface UTCSDiskQuotaAuthenticationViewController ()
-@property (nonatomic) UIView        *loginContainerView;
-@property (nonatomic) UITextField *usernameTextField;
-@property (nonatomic) UITextField *passwordTextField;
-@property (nonatomic) UTCSMenuButton    *menuButton;
+@property (nonatomic) UIView                    *loginContainerView;
+@property (nonatomic) UITextField               *usernameTextField;
+@property (nonatomic) UITextField               *passwordTextField;
+@property (nonatomic) UTCSMenuButton            *menuButton;
 @property (nonatomic) UIView                    *textFieldSeparatorView;
+@property (nonatomic) UIButton                  *loginButton;
 @end
 
 @implementation UTCSDiskQuotaAuthenticationViewController
@@ -32,8 +36,9 @@
     [super viewDidLoad];
     
      self.view.backgroundColor = [UIColor whiteColor];
-    
-    self.loginContainerView = [UIView new];
+    self.loginContainerView = [[UIView alloc]initWithFrame:self.view.bounds];
+    self.loginContainerView.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:self.loginContainerView];
     
     self.usernameTextField = ({
         UITextField *textField = [[UITextField alloc]initWithFrame:CGRectMake(0.0, 0.0, 0.8 * CGRectGetWidth(self.view.bounds), 44)];
@@ -80,13 +85,25 @@
     });
     [self.loginContainerView addSubview:self.passwordTextField];
     
-    [self.loginContainerView sizeToFit];
-    [self.view addSubview:self.loginContainerView];
-    
     // Menu Button
     self.menuButton = [[UTCSMenuButton alloc]initWithFrame:CGRectMake(2, 8, 56, 32)];
     self.menuButton.lineColor = [UIColor blackColor];
     [self.view addSubview:self.menuButton];
+    
+    self.loginButton = ({
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+        button.frame = CGRectMake(0.0, 0.0, 0.5 * self.view.width, 44.0);
+        button.center = CGPointMake(self.view.center.x, 1.5 * self.view.center.y);
+        button.layer.borderWidth = 1.0;
+        button.layer.borderColor = [UIColor blackColor].CGColor;
+        button.layer.cornerRadius = 10.0;
+        [button setTitle:@"Login" forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(didTouchUpInsideButton:) forControlEvents:UIControlEventTouchUpInside];
+        button;
+    });
+    [self.loginContainerView addSubview:self.loginButton];
+
 }
 
 - (void)textFieldDidBeginEditing:(UITextField *)textField
@@ -98,9 +115,38 @@
 
 - (void)textFieldDidEndEditing:(UITextField *)textField
 {
+    if(textField == self.passwordTextField) {
+        [UIView animateWithDuration:0.3 animations:^{
+            self.loginContainerView.center = self.view.center;
+        }];
+    }
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.view endEditing:YES];
+    
     [UIView animateWithDuration:0.3 animations:^{
-        self.loginContainerView.center = CGPointMake(self.view.center.x, self.view.center.y);
+        self.loginContainerView.center = self.view.center;
     }];
+}
+
+- (void)didTouchUpInsideButton:(UIButton *)button
+{
+    if(button == self.loginButton) {
+        BOOL success = [[UTCSSSHManager sharedSSHAuthHandler]connectWithUsername:self.usernameTextField.text
+                                                                        password:self.passwordTextField.text];
+        if(success) {
+            NSString *result = [[UTCSSSHManager sharedSSHAuthHandler]executeCommand:@"ls"];
+            NSLog(@"%@", result);
+            
+            // Store in keychain
+            // pop view controller
+        } else {
+            // shake window
+            [self.loginContainerView shake:3 withDelta:16.0];
+        }
+    }
 }
 
 @end
