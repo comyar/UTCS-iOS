4f7c4634c3b665df5a05e49b86445e7845f6a77e
Comyar <comyarzaheri@utexas.edu>
2014-04-05 13:36:26 -0500

    added comments to disk quota authentication
    Former-commit-id: 3153a75fcf24ca22ce2bb0086ce7773fe0cb5d8c
    Former-commit-id: f49194d329651a82e35d0e13c7ee9ab22ab1eaaa
    Former-commit-id: e6b4547c30a8d03def4157f4683a10c1f4bf1cff

--- UTCS/UTCS/Classes/ViewControllers/UTCSDiskQuotaAuthenticationViewController.h
+++ UTCS/UTCS/Classes/ViewControllers/UTCSDiskQuotaAuthenticationViewController.h
@@ -8,15 +8,28 @@
 
 #import <UIKit/UIKit.h>
 
-
+/**
+ */
 @protocol UTCSDiskQuotaAuthenticationViewControllerDelegate <NSObject>
 
+/**
+ 
+ */
 - (void)didAuthenticate;
 
 @end
 
+
+/**
+ UTCSDiskQuotaAuthenticationViewController allows a user to authenticate with his or her Unix username and password in order
+ to view their disk quota. A successful login will result in the username and password being stored by the UTCSAccountManager
+ for use throughout the rest of the app.
+ */
 @interface UTCSDiskQuotaAuthenticationViewController : UIViewController <UITextFieldDelegate>
 
+/**
+ 
+ */
 @property (nonatomic) id<UTCSDiskQuotaAuthenticationViewControllerDelegate> delegate;
 
 @end
