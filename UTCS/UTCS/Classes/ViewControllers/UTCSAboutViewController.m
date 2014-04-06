f4d7dce0b61013f23dace46bf0f116c50c83565e
Comyar <comyarzaheri@utexas.edu>
2014-04-06 05:59:44 -0500

    added initial about page
    Former-commit-id: efbf726936d17b36be7a195dde09d2185b9f1f0b
    Former-commit-id: 790b4ec2b02214f169f0378ce63b3f79d9ce5da7
    Former-commit-id: 115cb77ccbc0be766f3fb7668b4d021e7c8a59e5

--- UTCS/UTCS/Classes/ViewControllers/UTCSAboutViewController.m
+++ UTCS/UTCS/Classes/ViewControllers/UTCSAboutViewController.m
@@ -8,19 +8,18 @@
 
 #import "UTCSAboutViewController.h"
 #import "UIImage+CZTinting.h"
+#import "UIView+CZPositioning.h"
 
 @interface UTCSAboutViewController ()
-// Image view used to display the background image
-@property (nonatomic) UIImageView               *backgroundImageView;
+@property (nonatomic) UITextView *textView;
 @end
 
 @implementation UTCSAboutViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        self.title = @"About";
     }
     return self;
 }
@@ -29,12 +28,38 @@
 {
     [super viewWillAppear:animated];
     [self.navigationController setNavigationBarHidden:NO animated:YES];
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        self.view.alpha = 1.0;
+    }];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [UIView animateWithDuration:0.3 animations:^{
+        self.view.alpha = 0.0;
+    }];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
+    self.textView = ({
+        UITextView *textView = [[UITextView alloc]initWithFrame:CGRectMake(0, 44, self.view.width, self.view.height - 44.0)];
+        textView.backgroundColor = [UIColor clearColor];
+        textView.textColor = [UIColor whiteColor];
+        textView.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:16];
+        textView.editable = NO;
+        textView.textContainerInset = UIEdgeInsetsMake(0.0, 8.0, 0.0, 8.0);
+        [self.view addSubview:textView];
+        textView;
+    });
     
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"App-About" ofType:@"txt"];
+    NSString *content = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    self.textView.text = content;
 }
+
 @end
