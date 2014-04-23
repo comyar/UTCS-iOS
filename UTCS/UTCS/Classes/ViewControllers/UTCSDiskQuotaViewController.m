b939189fb66e497d41f3e15bac81e026502d0277
Comyar <comyarzaheri@utexas.edu>
2014-04-23 17:02:46 -0500

    added error/description labels to disk quota
    Former-commit-id: 2876a742e9ce3ec120c63bac7e28b6d7f80a8938
    Former-commit-id: 997ff7204a88b876e32f70d64880da553f415eeb
    Former-commit-id: 10ed316dd43a86978104561116129b4c81bcf4c4

--- UTCS/UTCS/Classes/ViewControllers/UTCSDiskQuotaViewController.m
+++ UTCS/UTCS/Classes/ViewControllers/UTCSDiskQuotaViewController.m
@@ -31,8 +31,6 @@ static NSString *diskQuotaCacheKey = @"quota";
 
 @interface UTCSDiskQuotaViewController ()
 
-@property (nonatomic) UITextField               *usernameTextField;
-
 @property (nonatomic) UTCSButton                *goButton;
 
 @property (nonatomic) UILabel                   *nameLabel;
@@ -43,9 +41,17 @@ static NSString *diskQuotaCacheKey = @"quota";
 
 @property (nonatomic) UILabel                   *updatedLabel;
 
-@property (nonatomic) UILabel                   *diskQuotaDetailLabel;
+@property (nonatomic) UILabel                   *quotaDetailLabel;
 
-@property (nonatomic) MRCircularProgressView    *diskQuotaGaugeView;
+@property (nonatomic) MRCircularProgressView    *quotaGaugeView;
+
+@property (nonatomic) UITextField               *usernameTextField;
+
+@property (nonatomic) UILabel                   *frownyFaceLabel;
+
+@property (nonatomic) UILabel                   *errorMessageLabel;
+
+@property (nonatomic) UILabel                   *descriptionLabel;
 
 @end
 
@@ -58,6 +64,106 @@ static NSString *diskQuotaCacheKey = @"quota";
 {
     if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
         self.dataSource = [[UTCSDiskQuotaDataSource alloc]initWithService:@"quota"];
+        
+        // Username text field
+        self.usernameTextField = ({
+            UITextField *textField = [[UITextField alloc]initWithFrame:CGRectZero];
+            textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+            textField.autocorrectionType = UITextAutocorrectionTypeNo;
+            textField.placeholder = @"CS Unix Username";
+            textField.textColor = [UIColor whiteColor];
+            textField.tintColor = [UIColor whiteColor];
+            textField;
+        });
+        
+        // Go button
+        self.goButton = ({
+            UTCSButton *button = [[UTCSButton alloc]initWithFrame:CGRectZero];
+            [button addTarget:self action:@selector(didTouchUpInsideButton:) forControlEvents:UIControlEventTouchUpInside];
+            
+            UIImageView *imageView = [[UIImageView alloc]initWithImage:[[UIImage imageNamed:@"goButton"]imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
+            imageView.tintColor = [UIColor whiteColor];
+            [button addSubview:imageView];
+            
+            button;
+        });
+        
+        // Quota circular view
+        self.quotaGaugeView = ({
+            MRCircularProgressView *view = [[MRCircularProgressView alloc]initWithFrame:CGRectZero];
+            view.backgroundColor = [UIColor clearColor];
+            view.progressColor = [UIColor whiteColor];
+            view.progressArcWidth = 8.0;
+            view.alpha = 0.0;
+            view;
+        });
+        
+        // Name label
+        self.nameLabel = ({
+            UILabel *label = [[UILabel alloc]initWithFrame:CGRectZero];
+            label.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:40];
+            label.textColor = [UIColor colorWithWhite:1.0 alpha:0.5];
+            label.textAlignment = NSTextAlignmentCenter;
+            label.adjustsFontSizeToFitWidth = YES;
+            label;
+        });
+        
+        // Quota detail label
+        self.quotaDetailLabel = ({
+            UILabel *label = [[UILabel alloc]initWithFrame:CGRectZero];
+            label.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:24];
+            label.textColor = [UIColor whiteColor];
+            label.textAlignment = NSTextAlignmentCenter;
+            label;
+        });
+        
+        // Unit label
+        self.unitLabel = ({
+            UILabel *label = [[UILabel alloc]initWithFrame:CGRectZero];
+            label.text = @"MB";
+            label.font = [UIFont fontWithName:@"HelveticaNeue" size:14];
+            label.textColor = [UIColor whiteColor];
+            label.textAlignment = NSTextAlignmentCenter;
+            label.alpha = 0.0;
+            label;
+        });
+        
+        // Updated label
+        self.updatedLabel = ({
+            UILabel *label = [[UILabel alloc]initWithFrame:CGRectZero];
+            label.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:14];
+            label.textAlignment = NSTextAlignmentCenter;
+            label.textColor = [UIColor colorWithWhite:1.0 alpha:0.5];
+            label;
+        });
+        
+        self.descriptionLabel = ({
+            UILabel *label = [[UILabel alloc]initWithFrame:CGRectZero];
+            label.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:16];
+            label.textColor = [UIColor colorWithWhite:1.0 alpha:0.5];
+            label.numberOfLines = 0;
+            label.text = @"Enter your CS Unix username to check your available disk quota.\n\nYour disk quota represents the amount of available disk space for your Unix account.";
+            label;
+        });
+        
+        self.frownyFaceLabel = ({
+            UILabel *label = [[UILabel alloc]initWithFrame:CGRectZero];
+            label.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:128];
+            label.textColor = [UIColor colorWithWhite:1.0 alpha:0.5];
+            label.textAlignment = NSTextAlignmentCenter;
+            label.text = @"☹";
+            label;
+        });
+        
+        self.errorMessageLabel = ({
+            UILabel *label = [[UILabel alloc]initWithFrame:CGRectZero];
+            label.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:16];
+            label.textColor = [UIColor colorWithWhite:1.0 alpha:0.5];
+            label.text = @"Ouch! Something went wrong.\n\nPlease check your Unix username and network connection.";
+            label.numberOfLines = 0;
+            label;
+        });
+        
     }
     return self;
 }
@@ -67,85 +173,60 @@ static NSString *diskQuotaCacheKey = @"quota";
     [super viewDidLoad];
     
     self.backgroundImageView.image = [UIImage imageNamed:@"diskQuotaBackground"];
-    
-    self.usernameTextField = ({
-        UITextField *textField = [[UITextField alloc]initWithFrame:CGRectMake(0.125 * self.view.width, 44.0, 0.5 * self.view.width, 44)];
-        textField.placeholder = @"CS Unix Username";
-        textField.tintColor = [UIColor whiteColor];
-        textField;
-    });
+
     [self.view addSubview:self.usernameTextField];
-    
-    self.goButton = ({
-        UTCSButton *button = [[UTCSButton alloc]initWithFrame:CGRectMake(0.0, 0.0, 44.0, 44.0)];
-        [button addTarget:self action:@selector(didTouchUpInsideButton:) forControlEvents:UIControlEventTouchUpInside];
-        
-        UIImageView *imageView = [[UIImageView alloc]initWithImage:[[UIImage imageNamed:@"goButton"]imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
-        imageView.tintColor = [UIColor whiteColor];
-        [button addSubview:imageView];
-        
-        button.center = CGPointMake(0.85 * self.view.width, self.usernameTextField.center.y);
-        button;
-    });
+
     [self.view addSubview:self.goButton];
     
-    self.diskQuotaGaugeView = ({
-        MRCircularProgressView *view = [[MRCircularProgressView alloc]initWithFrame:CGRectMake(0, 0, 0.625 * self.view.width,
-                                                                                               0.625 * self.view.width)];
-        view.center = self.view.center;
-        view.backgroundColor = [UIColor clearColor];
-        view.progressColor = [UIColor whiteColor];
-        view.progressArcWidth = 8.0;
-        view.alpha = 0.0;
-        [self.view addSubview:view];
-        view;
-    });
+    [self.view addSubview:self.quotaGaugeView];
     
-    self.nameLabel = ({
-        UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(0.0, 128.0, self.view.width, 24)];
-        label.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:20];
-        label.textColor = [UIColor colorWithWhite:1.0 alpha:0.5];
-        label.textAlignment = NSTextAlignmentCenter;
-        label.adjustsFontSizeToFitWidth = YES;
-        [self.view addSubview:label];
-        label;
-    });
+    [self.view addSubview:self.nameLabel];
     
-    self.diskQuotaDetailLabel = ({
-        UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(0.0, 0.0, 0.6 * self.view.width, 24)];
-        label.center = CGPointMake(self.view.center.x, self.view.center.y - self.view.y);
-        label.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:24];
-        label.textColor = [UIColor whiteColor];
-        label.textAlignment = NSTextAlignmentCenter;
-        [self.view addSubview:label];
-        label;
-    });
+    [self.view addSubview:self.quotaDetailLabel];
     
-    self.unitLabel = ({
-        UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(0.0, 0.0, 0.6 * self.view.width, 14)];
-        label.center = CGPointMake(self.view.center.x, 1.08 * self.view.center.y - self.view.y);
-        label.text = @"MB";
-        label.font = [UIFont fontWithName:@"HelveticaNeue" size:14];
-        label.textColor = [UIColor whiteColor];
-        label.textAlignment = NSTextAlignmentCenter;
-        label.alpha = 0.0;
-        [self.view addSubview:label];
-        label;
-    });
+    [self.view addSubview:self.unitLabel];
     
-    self.updatedLabel = ({
-        UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(0.0, self.view.height - 24, self.view.width, 24)];
-        label.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:14];
-        label.textAlignment = NSTextAlignmentCenter;
-        label.textColor = [UIColor colorWithWhite:1.0 alpha:0.5];
-        [self.view addSubview:label];
-        label;
-    });
+    [self.view addSubview:self.updatedLabel];
+    
+    [self.view addSubview:self.descriptionLabel];
+    
+    [self.view addSubview:self.frownyFaceLabel];
+    [self.view addSubview:self.errorMessageLabel];
+    
 }
 
-- (void)viewDidAppear:(BOOL)animated
+- (void)viewDidLayoutSubviews
 {
-    [super viewDidAppear:animated];
+    [super viewDidLayoutSubviews];
+    
+    self.usernameTextField.frame = CGRectMake(0.125 * self.view.width, 44.0, 0.5 * self.view.width, 44);
+    
+    self.goButton.frame = CGRectMake(0.0, 0.0, 44.0, 44.0);
+    self.goButton.center = CGPointMake(0.85 * self.view.width, self.usernameTextField.center.y);
+    
+    self.quotaGaugeView.frame = CGRectMake(0, 0, 0.625 * self.view.width, 0.625 * self.view.width);
+    self.quotaGaugeView.center = self.view.center;
+    
+    self.nameLabel.frame = CGRectMake(0.0, 128.0, self.view.width, 48);
+    
+    self.quotaDetailLabel.frame = CGRectMake(0.0, 0.0, 0.6 * self.view.width, 24);
+    self.quotaDetailLabel.center = CGPointMake(self.view.center.x, self.view.center.y - self.view.y);
+    
+    self.unitLabel.frame = CGRectMake(0.0, 0.0, 0.6 * self.view.width, 14);
+    self.unitLabel.center = CGPointMake(self.view.center.x, 1.08 * self.view.center.y - self.view.y);
+    
+    self.updatedLabel.frame = CGRectMake(0.0, self.view.height - 24, self.view.width, 24);
+    
+    
+    self.descriptionLabel.frame = CGRectMake(0.0, 0.0, 0.75 * self.view.width, 0.5 * self.view.width);
+    self.descriptionLabel.center = self.view.center;
+
+    
+    self.frownyFaceLabel.frame = CGRectMake(0.0, 0.0, 0.5 * self.view.width, 0.5 * self.view.width);
+    self.frownyFaceLabel.center = CGPointMake(self.view.center.x, 0.9 * self.view.center.y);
+    
+    self.errorMessageLabel.frame = CGRectMake(0.0, 0.0, 0.75 * self.view.width, 0.5 * self.view.width);
+    self.errorMessageLabel.center = CGPointMake(self.view.center.x, 1.24 * self.view.center.y);
 }
 
 - (void)didTouchUpInsideButton:(UIButton *)button
@@ -158,24 +239,23 @@ static NSString *diskQuotaCacheKey = @"quota";
 
 - (void)update
 {
-    NSLog(@"update");
     if ([self.dataSource shouldUpdate]) {
         MBProgressHUD *progressHUD = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
         progressHUD.mode = MBProgressHUDModeIndeterminate;
         progressHUD.labelText = @"Updating";
         
         [self updateWithArgument:self.usernameTextField.text completion:^(BOOL success) {
-            NSLog(@"quota completion");
+            
             if (success) {
                 self.nameLabel.text = self.dataSource.data[@"name"];
                 CGFloat limit = [self.dataSource.data[@"limit"]floatValue];
                 CGFloat usage = [self.dataSource.data[@"usage"]floatValue];
-                [self.diskQuotaGaugeView setProgress:(usage / limit) animated:YES];
-                self.diskQuotaDetailLabel.text = [NSString stringWithFormat:@"%0.0f / %0.0f", usage, limit];
+                [self.quotaGaugeView setProgress:(usage / limit) animated:YES];
+                self.quotaDetailLabel.text = [NSString stringWithFormat:@"%0.0f / %0.0f", usage, limit];
                 
                 [UIView animateWithDuration:0.3 animations:^{
-                    self.diskQuotaDetailLabel.alpha = 1.0;
-                    self.diskQuotaGaugeView.alpha = 1.0;
+                    self.quotaDetailLabel.alpha = 1.0;
+                    self.quotaGaugeView.alpha = 1.0;
                     self.unitLabel.alpha = 1.0;
                 }];
                 
@@ -184,8 +264,22 @@ static NSString *diskQuotaCacheKey = @"quota";
                                                                          timeStyle:NSDateFormatterMediumStyle];
                 self.updatedLabel.text = [NSString stringWithFormat:@"Updated %@", updatedString];
             } else {
-                NSLog(@"quota failed");
+                
+                self.updatedLabel.text = @"Update Failed";
+                
+                [UIView animateWithDuration:0.3 animations:^{
+                    self.frownyFaceLabel.alpha = 1.0;
+                    self.errorMessageLabel.alpha = 1.0;
+                    self.quotaGaugeView.alpha = 0.0;
+                    self.quotaDetailLabel.alpha = 0.0;
+                    self.unitLabel.alpha = 0.0;
+                }];
+                
             }
+            
+            [UIView animateWithDuration:0.3 animations:^{
+                self.descriptionLabel.alpha = 0.0;
+            }];
             
             [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
         }];
@@ -195,24 +289,6 @@ static NSString *diskQuotaCacheKey = @"quota";
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [self.view endEditing:YES];
-}
-
-
-#pragma mark UITextFieldDelegate Methods
-
-- (void)textFieldDidBeginEditing:(UITextField *)textField
-{
-
-}
-
-- (void)textFieldDidEndEditing:(UITextField *)textField
-{
-
-}
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField
-{
-    return YES;
 }
 
 @end
--- UTCS/UTCS/UTCSAppDelegate.m
+++ UTCS/UTCS/UTCSAppDelegate.m
@@ -93,7 +93,7 @@
     self.diskQuotaViewController        = [UTCSDiskQuotaViewController new];
     
     self.verticalMenuViewController     = [[UTCSVerticalMenuViewController alloc]initWithMenuViewController:self.menuViewController
-                                                                                      contentViewController:self.newsNavigationController];
+                                                                                      contentViewController:self.diskQuotaViewController];
     
     self.window.rootViewController = self.verticalMenuViewController;
     [self.window makeKeyAndVisible];
