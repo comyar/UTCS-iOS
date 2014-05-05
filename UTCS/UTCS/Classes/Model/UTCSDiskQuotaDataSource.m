f0423cbd1f261500fe9cea4bb1cbacd028333fb2
Comyar <comyarzaheri@utexas.edu>
2014-05-05 08:23:40 -0500

    removed disk quota cache
    Former-commit-id: 5502b0dd7f5eee118011af34bee39fde7d19fdbd
    Former-commit-id: b86c9836d5cb86eec66681b3ca3cdbe0b97f6046
    Former-commit-id: 217ad6863a3b54bcb692f91bf41bf04857c2d85d

--- UTCS/UTCS/Classes/Model/UTCSDiskQuotaDataSource.m
+++ UTCS/UTCS/Classes/Model/UTCSDiskQuotaDataSource.m
@@ -27,13 +27,7 @@ NSString * const UTCSDiskQuotaDataSourceCacheKey = @"UTCSDiskQuotaDataSourceCach
 - (instancetype)initWithService:(NSString *)service
 {
     if (self = [super initWithService:service]) {
-        _minimumTimeBetweenUpdates = 10800.0;  // 3 hours 
         _parser = [UTCSDiskQuotaDataSourceParser new];
-        _cache = [[UTCSDataSourceCache alloc]initWithService:service];
-        
-        // Restore cached disk quota information
-        NSDictionary *cache = [_cache objectWithKey:UTCSDiskQuotaDataSourceCacheKey];
-        _data = cache[UTCSDataSourceCacheValuesName];
     }
     return self;
 }
