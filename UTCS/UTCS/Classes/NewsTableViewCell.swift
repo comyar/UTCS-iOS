283f4e31cfb3936c89c178bd4faef83dbc85dc59
Jesse Tipton <me@jessetipton.com>
2015-12-16 02:36:20 -0600

    minimized NewsTableViewCell

--- UTCS/UTCS/Classes/NewsTableViewCell.swift
+++ UTCS/UTCS/Classes/NewsTableViewCell.swift
@@ -1,24 +1,13 @@
-import Foundation
-
-// Name of the image to use for a table view cell's accessory view.
-let cellAccessoryImageName = "rightArrow"
 
 class NewsTableViewCell: BouncyTableViewCell {
+    
     @IBOutlet var title: UILabel!
     @IBOutlet var detailLabel: UILabel!
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        accessoryView = {
-            let image = UIImage(named: cellAccessoryImageName)?.imageWithRenderingMode(.AlwaysTemplate)
-            let imageView = UIImageView(image: image)
-            imageView.tintColor = UIColor(white: 1.0, alpha: 0.5)
-            return imageView
-        }()
-
-
+        accessoryType = .DisclosureIndicator
+        backgroundColor = .clearColor()
     }
-    override func awakeFromNib() {
-        backgroundColor = UIColor.clearColor()
-    }
+
 }
