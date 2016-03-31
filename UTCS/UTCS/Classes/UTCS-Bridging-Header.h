d7ba16e82a9f40f8ad6020205e38b1a25f965d17
Jesse Tipton <me@jessetipton.com>
2016-03-31 18:02:29 -0500

    removed unused imports from bridging header

--- UTCS/UTCS/Classes/UTCS-Bridging-Header.h
+++ UTCS/UTCS/Classes/UTCS-Bridging-Header.h
@@ -1,7 +1,5 @@
+
 #import <CommonCrypto/CommonCrypto.h>
-#import <MBProgressHUD/MBProgressHUD.h>
-#import <POP/POP.h>
 
 #import "UTCSLabMachine.h"
-#import "UTCSLabMachineView.h"
 #import "UTCSLabMachineViewController.h"
