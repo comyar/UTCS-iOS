ba970a4d930ce7e11c8df2daff0dff2dbb67f57f
Nick Walker <nick@nickwalker.us>
2016-04-01 15:27:13 -0500

    Update settings appearance to match rest of app
    Add MAD link in About section

--- UTCS/UTCS/Classes/ViewControllers/AboutViewController.swift
+++ UTCS/UTCS/Classes/ViewControllers/AboutViewController.swift
@@ -1,12 +1,10 @@
-
 import UIKit
 
-class AboutViewController: AutomaticDimensionTableViewController {
-
+class AboutViewController: PhotoBackgroundTableViewController {
     @IBOutlet weak var versionLabel: UILabel!
-    
     @IBOutlet weak var buildLabel: UILabel!
-    
+    let MAD_LINK = NSURL(string: "https://www.cs.utexas.edu/users/mad/")!
+
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -14,5 +12,11 @@ class AboutViewController: AutomaticDimensionTableViewController {
         versionLabel.text = application.version
         buildLabel.text = application.build
     }
-    
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        guard indexPath.section == 0 && indexPath.section == 1 else { return }
+        UIApplication.sharedApplication().openURL(MAD_LINK)
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+
 }
